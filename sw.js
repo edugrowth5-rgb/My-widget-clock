643a76d7275fb9087311ef1371684daae2a863dd
edugrowth5-rgb <edugrowth5@gmail.com>
2026-02-23 20:17:40 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,36 @@
-const cacheName = 'sagar-royal-v22';
-const assets = ['./', './index.html', './manifest.json'];
+const CACHE_NAME = 'sagar-clock-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './IMG-20260223-WA0000.jpg'
+];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+// Install Service Worker and cache assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+// Fetch assets from cache if offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Activate and remove old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
 });
